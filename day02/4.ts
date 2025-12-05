b47bce3292f9635615ed57e68dae3ba676988daa
graham-ely <firstclass140@gmail.com>
2025-12-04 23:47:46 -0500

    Minor code cleanup Day 2

--- day02/4.ts
+++ day02/4.ts
@@ -10,36 +10,34 @@ var invalid_sum: number = 0;
 var invalid_ids: string[] = [];
 
 for(const instr of data_arr){
-    const first_id = instr.substring(0, instr.indexOf('-'));
-    const second_id = instr.substring(instr.indexOf('-') + 1);
+    const first_id = parseInt(instr.substring(0, instr.indexOf('-')));
+    const second_id = parseInt(instr.substring(instr.indexOf('-') + 1));
 
-    // console.log(instr, first_id, second_id)
-
-    for( let i = parseInt(first_id); i <= parseInt(second_id); i++ ) {
+    for( let i = first_id; i <= second_id; i++ ) {
         var curr_id: string = i.toString();
         var curr_id_len: number = curr_id.length;
         var is_repeating: boolean = true;
 
         //only has to run up to half
-        for( let j = 0; j < (curr_id_len / 2) + 1; j++ ) {
-            if( curr_id_len % j == 0 && curr_id_len > 1 ) {
+        for(let j = 0; j < (curr_id_len / 2) + 1; j++) {
+            // took 10+ minutes to catch the single-digit case, very unfortunate
+            if(curr_id_len % j == 0 && curr_id_len > 1) {
                 var p1 = curr_id.substring(0, j);
                 var p2 = curr_id.substring(j, 2 * j);
                 var p_i = 2 * j;
 
                 while(is_repeating && p_i <= curr_id_len) {
                     //console.log("curr_id", curr_id, "p1:", p1, "p2:", p2)
-                    if( p1 != p2 ) {
+                    if(p1 != p2) {
                         is_repeating = false;
                     } else {
                         p1 = p2;
                         p2 = curr_id.substring(p_i, p_i + j);
                         p_i = p_i + j;
                     }
-                    //console.log(is_repeating)
                 }
 
-                if( is_repeating ) {
+                if(is_repeating) {
                     if(!invalid_ids.includes(curr_id)) {
                         invalid_sum += i;
                         invalid_ids.push(curr_id);
@@ -48,19 +46,10 @@ for(const instr of data_arr){
 
                 is_repeating = true;
             }
-
         }
-
-        //invalid_ids = [];
-
-        //console.log(first_half_id, second_half_id);
     }
-
 }
 
 console.log(data_arr);
 console.log(invalid_ids);
 console.log(invalid_sum);
-
-
-//27469417443 too high
