7b432694cb6bbdd3f1cad043988014872a4ea28e
graham-ely <firstclass140@gmail.com>
2025-12-07 21:06:40 -0500

    Day 3 correct

--- day03/5.ts
+++ day03/5.ts
@@ -1,17 +1,40 @@
-//  from start of 5
+// 21:38 from start of 5
 import * as fs from 'fs';
 
-const data_path: string = "5_data_test.txt";
-//const data_path: string   = "5_data.txt";
+//const data_path: string = "5_data_test.txt";
+const data_path: string   = "5_data.txt";
 const data: string = fs.readFileSync(data_path, 'utf-8');
 const data_arr: string[] = data.split(/\r?\n/);
 
-var sum: number = 0;
+var sum_j: number = 0;
 var ids: string[] = [];
 
-for(const instr of data_arr){
+for(const bank of data_arr){
+    var largest_bank: number = 0;
+    for(let i = 9; i >= 0; i--) {
+        var first_index = bank.indexOf(i.toString());
+        if(first_index >= 0) {
+            for(let j = 9; j >= 0; j--) {
+                const sub_bank   = bank.substring(first_index + 1);
+                var second_index = sub_bank.indexOf(j.toString());
+
+                if(second_index >= 0) {
+                    const sub_bank_total = Number(i.toString() + j.toString());
+
+                    if( sub_bank_total > largest_bank ) {
+                        largest_bank = sub_bank_total
+                    }
+                }
+            }
+        }
+    }
+
+    sum_j += largest_bank;
+
+    console.log(largest_bank, sum_j);
+
 }
 
 console.log(data_arr);
 console.log(ids);
-console.log(sum);
+console.log(sum_j);
