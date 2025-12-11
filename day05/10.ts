6c50dbb0fe44ac2ccbf6453183bd6bdb244107d2
graham-ely <firstclass140@gmail.com>
2025-12-10 22:58:17 -0500

    Day 5 correct This one got me, first step was mega easy and then the second caused me real trouble Had a few false starts and then just couldn't find the last edge cases for awhile Thought sort was expensive so I waited a really long time to add it? Unsure why I thought that

---
+++ day05/10.ts
@@ -0,0 +1,109 @@
+// 2:11:14 from start of 9 :(
+import * as fs from 'fs';
+
+//const data_path: string = "9_data_test.txt";
+const data_path: string   = "9_data.txt";
+const data: string = fs.readFileSync(data_path, 'utf-8');
+const data_arr: string[] = data.split(/\r?\n/);
+
+var sum: number = 0;
+var split_index = data_arr.indexOf("");
+
+var fresh_ranges_raw: string[] = data_arr.slice(0, split_index);
+//var ingredients: string[]  = data_arr.slice(split_index + 1);
+var fresh_ranges: Array<[number, number]> = [];
+var fresh_ranges_original: Array<[number, number]> = [];
+var combined_ranges: Array<[number, number]> = [];
+
+// process input array
+for(const fresh_range of fresh_ranges_raw) {
+    var slice_index = fresh_range.indexOf("-");
+    var range_start = Number(fresh_range.slice(0, slice_index));
+    var range_end   = Number(fresh_range.slice(slice_index + 1));
+
+    fresh_ranges.push([range_start, range_end]);
+    fresh_ranges_original.push([range_start, range_end]);
+}
+
+var prev_len = -1;
+
+while(prev_len != fresh_ranges.length) {
+    prev_len = combined_ranges.length;
+    combined_ranges = [];
+
+    for(const fresh_range of fresh_ranges) {
+        if(combined_ranges.length == 0) {
+            combined_ranges.push([...fresh_range]);
+        } else {
+            var push_new: boolean      = true;
+            var duplicate_val: boolean = false;
+
+            for(let [idx, existing_range] of combined_ranges.entries()) {
+                if(fresh_range[0] > existing_range[1] || fresh_range[1] < existing_range[0]) {
+                    // not within current bounds, could be a new range
+                } else if (fresh_range[0] < existing_range[0]) {
+                    if(fresh_range[1] < existing_range[1]) {
+                        combined_ranges[idx][0] = fresh_range[0];
+                        push_new = false;
+                    } else {
+                        combined_ranges[idx] = [...fresh_range];
+                        push_new = false;
+                    }
+                } else if (fresh_range[0] >= existing_range[0]) {
+                    if(fresh_range[1] <= existing_range[1]) {
+                        // entirely within existing range, no push
+                        push_new = false;
+                    } else {
+                        combined_ranges[idx][1] = fresh_range[1];
+                        push_new = false;
+                    }
+                } else if (fresh_range[0] == existing_range[0] && fresh_range[1] == existing_range[1]) {
+                    // flag duplicate for removal
+                    push_new = false;
+                    duplicate_val = true;
+                }
+
+            }
+
+            if(push_new == true) {
+                combined_ranges.push([...fresh_range]);
+            } else if(duplicate_val == true) {
+                //combined_ranges.splice(combined_ranges.indexOf(fresh_range), 1);
+            }
+        }
+    }
+
+    fresh_ranges = [];
+    combined_ranges.forEach((subArray) => fresh_ranges.push([...subArray]));
+
+}
+
+combined_ranges.sort(compare);
+
+
+for(const range of combined_ranges) {
+    sum += ( range[1] - range[0] + 1 );
+}
+
+
+
+function compare(a:[number, number], b:[number, number]) {
+    if(a[0] < b[0]) {
+        return -1;
+    } else if (a[0] > b[0]){
+        return 1;
+    }
+
+    return 1;
+}
+
+console.log(data_arr);
+console.log(fresh_ranges_original)
+console.log(combined_ranges);
+console.log(sum);
+
+//353580677618840 too high
+//349685392216382 too high
+//343377710090112 too high
+//336790092076620
+//334284659810868 too low
--- day05/9.ts
+++ day05/9.ts
@@ -1,4 +1,4 @@
-// from start of 9
+// 9:05 from start of 9
 import * as fs from 'fs';
 
 //const data_path: string = "9_data_test.txt";
@@ -9,16 +9,39 @@ const data_arr: string[] = data.split(/\r?\n/);
 var sum: number = 0;
 var log_arr: string[] = [];
 
-//for(const instr of data_arr){
-//}
-for(let i = 0; i < data_arr[0].length; i++) {
+var split_index = data_arr.indexOf("");
+
+var fresh_ranges: string[] = data_arr.slice(0, split_index);
+var ingredients: string[] = data_arr.slice(split_index + 1);
+
+for(const ingredient of ingredients){
+    if(is_ingredient_fresh(Number(ingredient), fresh_ranges)) {
+        sum++;
+    }
 }
+//for(let i = 0; i < ingredients.length; i++) {
+//}
 
 
-function func(x:number, y:number, arr: string[]) {
-    return 0;
+function is_ingredient_fresh(ingredient:number, fresh_ranges:string[]) {
+    var is_fresh: boolean = false;
+
+    for(const fresh_range of fresh_ranges) {
+        var slice_index = fresh_range.indexOf("-");
+        var range_start = Number(fresh_range.slice(0, slice_index));
+        var range_end   = Number(fresh_range.slice(slice_index + 1));
+
+        if(ingredient >= range_start && ingredient <= range_end) {
+            return true;
+        }
+
+    }
+
+    return false;
 }
 
 console.log(data_arr);
+console.log(ingredients);
+console.log(fresh_ranges);
 console.log(log_arr);
 console.log(sum);
